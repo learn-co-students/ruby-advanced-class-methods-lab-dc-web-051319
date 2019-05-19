40e3d8ed90e60152c8b7769a95eadb33cb365022
Robert Pedersen <rob@worldcrimeleague.org>
2019-05-19 16:51:26 -0400

    done

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,70 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(song_name)
+    song = Song.new
+    song.name = song_name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    # song = Song.new
+    # song.save
+    song = self.create
+    song.name = song_name
+    song
+  end
+
+  def self.find_by_name(song_name)
+    self.all.find do |song_instance|
+      song_instance.name == song_name
+    end
+  end
+
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by do |song|
+      song.name
+    end
+  end
+
+
+
+  def self.new_from_filename(filename)
+    file_parts = filename.split(/[-.]/)
+    artist = file_parts[0].strip
+    song_title = file_parts[1].strip
+
+    song = Song.new
+    song.name = song_title
+    song.artist_name = artist
+    song
+
+  end
+
+  def self.create_from_filename(filename)
+    file_parts = filename.split(/[-.]/)
+    artist = file_parts[0].strip
+    song_title = file_parts[1].strip
+
+    song = Song.create
+    song.name = song_title
+    song.artist_name = artist
+    song
+
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
 end
